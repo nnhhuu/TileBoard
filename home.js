e8b3873f9c5ef4ac24bf8b8874cf701210431734
nnhhuu <42612189+nnhhuu@users.noreply.github.com>
2018-11-08 08:51:42 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,60 @@
+var p_main = {
+   title: 'Main page',
+   bg: 'images/bg1.jpeg',
+   icon: 'mdi-home-automation', // home icon
+   groups: [
+
+// groupo 1
+      {
+        title: ' ',
+         width: 6,
+         height: 4,
+         items: [
+              main_text             (0,0),
+              garage_camera         (2,0),
+
+              //nhu_tracker           (0,1),
+              home_alarm            (1,1),
+              atlantic_camera       (2,1),
+
+//              status_group_ligths   (0,2),
+//              sensor_group          (1,2),
+              underground_camera    (2,2),
+
+              t_screensaver         (4,0),
+              t1_screensaver        (4,1),
+              f_reload              (4,2),
+
+            //  t_weather      (0,0),
+        ]
+      }
+
+    ]
+};
+
+var p_main_m = {
+   title: 'Main page',
+   bg: 'images/bg1.jpeg',
+   icon: 'mdi-home-automation', // home icon
+   groups: [
+
+// groupo 1
+      {
+        title: ' ',
+         width: 3,
+         height: 6,
+         items: [
+              main_text             (0,0),
+              garage_camera         (2,0),
+
+              nhu_tracker           (0,1),
+              home_alarm            (1,1),
+              atlantic_camera       (2,1),
+
+              underground_camera    (2,2),
+
+        ]
+      }
+
+    ]
+};
